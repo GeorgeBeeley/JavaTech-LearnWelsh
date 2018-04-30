c257d55d3b25c6567869a51e4809ba1c7e13fdf3
GeorgeBeeley <george.beeley@gmail.com>
2018-04-30 14:00:25 +0100

    included jacks files in updated project structure

---
+++ WelshAcademy/src/main/java/questions/QuestionGenerator.java
@@ -0,0 +1,33 @@
+package questions;
+
+import java.util.Random;
+
+/**
+ * generates random questions using a translated pair of words (1 welsh, 1 English)
+ * @author Jack
+ */
+public final class QuestionGenerator {
+    
+    private static final String[] QUESTIONS = 
+        {"What is the gender of the Welsh noun ",
+        "What is the meaning of the Welsh noun ",
+        "What is the Welsh noun for the English word "};
+    private static final Random RAND = new Random();
+    
+    public static String newQuestion(Translation t){
+        int r = RAND.nextInt(3);
+        String question = QUESTIONS[r];
+        switch(r){
+            case 0:
+                question = question + t.WELSH;
+                break;
+            case 1:
+                question = question + t.WELSH;
+                break;
+            case 2:
+                question = question + t.ENG;
+                break;
+        }
+        return question;
+    }
+}
